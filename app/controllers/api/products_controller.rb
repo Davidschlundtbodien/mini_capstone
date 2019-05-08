e851eb5a8e6520527d7ca8e32769ac4746958a51
davidschlundtbodien <d.schlundtbodien@gmail.com>
2019-05-08 12:28:57 -0700

    adds routes for indivdual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jbuilder'
   end
+
+  def saison
+    @product = Product.find_by(name: "Saison")
+    render 'beer_info.json.jbuilder'
+  end
+
+  def bier_de_garde
+    @product = Product.find_by(name: "Bier de garde")
+    render 'beer_info.json.jbuilder'
+  end
+
+  def gueuze
+    @product = Product.find_by(name: "Gueuze")
+    render 'beer_info.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products" => "products#all_products_action"
+    get "/saison" => "products#saison"
+    get "/bier_de_garde" => "products#bier_de_garde"
+    get "/gueuze" => "products#gueuze"
   end
 end
